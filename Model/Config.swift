99a62d51abb4791933c8b70323df8361e995a2bb
Jy <jayluxferro@sperixlabs.org>
2021-08-07 14:06:21 +0000

    updated c2

--- Model/Config.swift
+++ Model/Config.swift
@@ -11,7 +11,7 @@ struct Config {
     static let networks = ["MTN", "Vodafone", "AirtelTigo"]
     static let phoneNumber = Utils.parseConfig().object(forKey: "phone") as! String
     static let meterNumber = "14152852"
-    static let voucher = ""
+    static let voucher = "123456"
     static let amount = "1"
     static let log = ""
     static let isProcessing = false
